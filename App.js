3dda5d20bd950b6235644dbc3c9f844eaadd226e
Joel <itsjoelpm@gmail.com>
2023-10-12 14:05:39 +0300

    init app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import styles from './App.styles';
+import { View, Alert, Text, ActivityIndicator } from 'react-native';
+import { useEffect, useState } from 'react';
+import Header from './src/components/Header';
+import questions from './assets/data/allQuestions';
+import OpenEndedQuestion from './src/components/OpenEndedQuestion';
+
+import ImageMultipleChoiceQuestion from './src/components/ImageMultipleChoiceQns';
+
+
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+
+  useEffect(() => {
+    if (currentQuestionIndex >= questions.length) {
+      Alert.alert('YOU WIN🎉🎉🎉🎉');
+      setCurrentQuestionIndex(0);
+    } else {
+      setCurrentQuestion(questions[currentQuestionIndex]);
+    }
+  }, [currentQuestionIndex])
+
+  function correctAns() {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  }
+
+  function wrongAns() {
+    Alert.alert('Ooooops....Try Again')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      {currentQuestion.type === 'IMAGE_MULTIPLE_CHOICE' ? (
+        <ImageMultipleChoiceQuestion currentQuestion={currentQuestion} onCorrect={correctAns} onWrong={wrongAns} />
+      ) : (
+        <OpenEndedQuestion currentQuestion={currentQuestion} onCorrect={correctAns} onWrong={wrongAns} />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
